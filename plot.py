f260c8285e32ba7aecc2a8a6ac21b713bf670cba
sebranchett <s.e.branchett@tudelft.nl>
2020-05-27 12:03:43 +0200

    corrected plot y axis

--- plot.py
+++ plot.py
@@ -5,9 +5,13 @@ with open('output.csv', newline='') as f:
     data_reader = reader(f)
     data = list(data_reader)
 
-temps = [item[1] for item in data]
+temps = [float(item[1]) for item in data]
 times = [item[0] for item in data]
-plt.plot(times, temps)
+print(temps)
+print(times)
+plt.plot(times, temps, marker = 'o')
 
-# plt.ylim((20,80))
+plt.xticks(times)
+plt.yticks(temps)
+plt.axis([None, None, 0, 40])
 plt.show()
