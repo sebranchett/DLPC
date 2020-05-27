4a140f63b16b1ec8eb6f441a0f8f2d7a79eace8a
sebranchett <s.e.branchett@tudelft.nl>
2020-05-27 16:00:25 +0200

    basis of graph complete

--- plot.py
+++ plot.py
@@ -2,19 +2,28 @@ import matplotlib.pyplot as plt
 from csv import reader
 from datetime import datetime, timedelta
 
+# read time and temperature data from a csv file
 with open('output.csv', newline='') as f:
     data_reader = reader(f)
     data = list(data_reader)
 
+# convert temperatures to real numbers
 temps = [float(item[1]) for item in data]
-times = [item[0] for item in data]
-plt.plot(times, temps, marker = 'o')
 
-print(times)
-lower_date = datetime.strptime(times[0],"%X").replace(second=0,microsecond=0)
-date_list = [lower_date + timedelta(minutes=x) for x in range(3)]
-time_list = [str(x.time()) for x in date_list]
-print(time_list)
-plt.xticks(time_list)
+# convert times to seconds from start of first minute
+time_labels = [item[0] for item in data]
+lower_date = datetime.strptime(time_labels[0],"%X").replace(second=0,microsecond=0)
+time_vals = [(datetime.strptime(i,"%X") - lower_date).seconds for i in time_labels]
+
+# create convenient tick marks and their labels
+tick_at = []
+tick_label = []
+for step in range(3):
+    tick_at.append(0 + step*60)
+    tick_label.append(str((lower_date + timedelta(seconds=60*step)).time()))
+
+# plot the data
+plt.plot(time_vals, temps, marker = 'o')
+plt.xticks(tick_at,tick_label)
 plt.yticks(range(0, 81, 10))
 plt.show()
