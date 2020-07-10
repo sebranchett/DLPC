62b2e11ae668a8f3292d2161941d7db8c7e4b129
sebranchett <s.e.branchett@tudelft.nl>
2020-07-10 12:08:29 +0200

    increasing no. of points script modified on DLPC

--- calc_pi/no_random_pairs.sh
+++ calc_pi/no_random_pairs.sh
@@ -1,7 +1,8 @@
 for pairs in 10 100 1000 10000 100000 1000000
 do
-    mpiexec -n 1 python calculate_pi.py $pairs > r1p"$pairs".out
+    mpiexec -hostfile myhostfile$ranks -np 1 python3 blog/calc_pi/calculate_pi.py $pairs > r1p"$pairs".out
     mv pi_estimate_10*_points.csv r1p"$pairs".csv
     mv pi_estimate_10*_points.png r1p"$pairs".png
     tail -n 3 r1p"$pairs".out
+    sleep 2
 done
